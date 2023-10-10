4f79f7f8f326f0d06dc3bec92ef04f7bee6f40d2
Shriyansh Bhargava <63575417+shriyanshbhargava@users.noreply.github.com>
2023-10-10 21:03:02 +0530

    made about page responsive

--- src/components/navBar/navbar.jsx
+++ src/components/navBar/navbar.jsx
@@ -1,23 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import {
+  AlignCenterVertical,
   FacebookLogo,
   InstagramLogo,
   MapPin,
+  PhoneCall,
   ShoppingCart,
+  SlidersHorizontal,
   WhatsappLogo,
 } from "phosphor-react";
 import "./navbar.css";
 
 export const Navbar = () => {
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 768px)").matches
+  );
+
+  useEffect(() => {
+    window
+      .matchMedia("(min-width: 768px)")
+      .addEventListener("change", (e) => setMatches(e.matches));
+  }, []);
+
   return (
     <div className="Navbar">
       <div className="topNavbar">
-        <h7>
-          {" "}
-          <MapPin /> Near Jaihindpuram market, Satya Sai Nagar, TVS Nagar,
-          Madurai-11 <WhatsappLogo /> +91 96299 75555
-        </h7>
+        <div>
+          {matches && (
+            <h7>
+              {" "}
+              <MapPin /> Near Jaihindpuram market, Satya Sai Nagar, TVS Nagar,
+              Madurai-11 <WhatsappLogo /> +91 96299 75555
+            </h7>
+          )}
+          {!matches && (
+            <div className="mobileTopNav text-white">
+              {" "}
+              <a href="https://api.whatsapp.com/send?phone=919629975555">
+                <WhatsappLogo size={25} color="white" />
+              </a>
+              |
+              <a href="tel:9196299 75555">
+                <PhoneCall size={25} color="white" />
+              </a>
+            </div>
+          )}
+        </div>
       </div>
       <div className="middleNavbar">
         <img
@@ -33,7 +62,7 @@ export const Navbar = () => {
           <Link to="/shop"> Products </Link>
           <Link to="/contact"> Contact </Link>
           <Link to="/cart">
-           Cart <ShoppingCart className="logoCart" />
+            Cart <ShoppingCart className="logoCart" />
           </Link>
           <Link to="/admin/signin"> SignIn </Link>
         </div>
